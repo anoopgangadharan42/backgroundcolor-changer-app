c1f0d1529e3994805da98d7fabaf53a59aeff8a2
anoopgangadharan42 <anoop42ah@gmail.com>
2024-02-15 00:44:26 +0530

    backgroundcolor changerapp

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,29 @@
+import { useState,useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const handleclickfunction =(e)=>{
-   const body =document.querySelector('body');
-   body.style.background= getRandomColor()
-   e.target.style.backgroundcolor =getRandomColor()
-  };
-
-    function getRandomColor(){
-      let letters ='0123456789ABCDEF'
-      let color='#'
-      for(let i =0; i<6; i++){
-        color+=letters[Math.floor(Math.random() * 16)];
-      }
-      return color;
-    }
+  const[background, setbackground] =useState()
+  useEffect(()=>{
+    document. body.className=background
+  },[background])
+  const handleFunction =(color)=>{
+    setbackground(color)
+  }
+ 
   return (
     <>
-     <div className="container">
-     <h1>Random Background color changer</h1>
-     <button className='btn' onClick={handleclickfunction}> click me</button>
-
-     </div>
+    <div  style={{width:'100%',height:'100vh'}}className="d-flex justify-content-center align-item-center" >
+      <div className="rounded p-3">
+      <h1>Random Background color changer</h1>
+      <div style={{marginTop:'20px' }} className="text-center">
+        <button onClick={()=>handleFunction('bg-danger')}type='button' className='btn btn-danger'>red</button>
+        <button onClick={()=>handleFunction('bg-warning')}type='button' className='btn btn-warning'>yellow</button>
+        <button onClick={()=>handleFunction('bg-primary')}type='button' className='btn btn-primary'>blue</button>
+        <button onClick={()=>handleFunction('bg-text-success')}type='button' className='btn btn-success'>green</button>
+      </div>
+    </div>
+    </div>
+     
      
    
     </>
